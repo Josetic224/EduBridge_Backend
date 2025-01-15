38bc052004cc712850b6a8376fe356673aa7a4ff
Josetic224 <ochiaghaifeanyichukwu185@gmail.com>
2025-01-15 03:02:42 +0100

    fixed Log Levels

--- utils/logger.util.js
+++ utils/logger.util.js
@@ -1,7 +1,7 @@
 const pino = require("pino");
 const expressPinoLogger = require("express-pino-logger");
 
-const logger = pino({ level: process.env.LOG_LEVEL});
+const logger = pino({ level: process.env.LOG_LEVEL || "info" });
 
 module.exports = {
   expressPinoLogger,
